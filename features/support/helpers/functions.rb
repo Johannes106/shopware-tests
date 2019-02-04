29d546267c6127d9329fd83b25f13efe6a417304
johannes112 <johannes.emmos@gmail.com>
2019-02-04 14:34:10 +0100

    extension: integrated new functions of browsertests

--- features/support/helpers/functions.rb
+++ features/support/helpers/functions.rb
@@ -334,23 +334,16 @@ module MyFunctions
       # if element is not found
     rescue Capybara::ElementNotFound => e
       #wrong path or required data
-      if(page.has_css?(".has--error"))
-        begin # identiy kind of error: check if element has class 'has--error'
-          find(".has--error")
-          puts "\033[35m>>> There is a errormessage\033[0m\n"
-          puts "\033[35m#{e.inspect}\033[0m\n"
-          # are there several elements of this errortyoe exist
-        rescue Capybara::Ambiguous => e
-          puts "\033[35m#{e.inspect}\033[0m\n"
-          puts "\033[45m  >>> Several errors exist\033[0m\n"
-          begin # identiy kind of error: check if element has class '.is--required.has--error'
-            find(".is--required.has--error")
-          rescue Capybara::Ambiguous => e
-            puts "\033[45m  >>> several inputs are required\033[0m\n"
-            element = find(".is--required.has--error")
-            puts "\033[31m    >>>>>> #{element[:class]}\033[0m\n"
-          end
-        end
+      puts "Some elements not existing on the page"
+      puts "'#{path}' is not found"
+      # look if there react-elements
+      if (page.html).include?('/react/')
+        puts "it is a page with content of react"
+        find_element_with_scrolling(path)
+      elsif(page.has_css?(".has--error"))
+        exception_has_error
+      elsif(page.has_css?('.entry--menu'))
+        path = '.entry--menu'
       end
       # search for similar path and rerun with new path given of function
       find_secure_counter < 2 ? retry : raise
@@ -359,14 +352,8 @@ module MyFunctions
       puts "\033[35m#{e.inspect}\033[0m\n"
       sleep 1
       puts "find_secure_with_one_arg"
-      Capybara.default_max_wait_time = 20
-      if url
-        puts "visit #{url} again"
-        visit(url)
-        puts "Failed to visit #{url}, retry #{find_secure_counter}"
-      else
-        puts "no URL IS Defined"
-      end
+      #Capybara.default_max_wait_time = 20
+      reload_page(url)
       find_secure_counter <= 2 ? retry : raise
     rescue Selenium::WebDriver::Error::UnhandledAlertError
       puts "\033[35m#{e.inspect}\033[0m\n"
@@ -382,6 +369,26 @@ module MyFunctions
     end
     return found
   end
+
+  def exception_has_error
+    begin # identiy kind of error: check if element has class 'has--error'
+      find(".has--error")
+      puts "\033[35m>>> There is a errormessage\033[0m\n"
+      puts "\033[35m#{e.inspect}\033[0m\n"
+      # are there several elements of this errortyoe exist
+    rescue Capybara::Ambiguous => e
+      puts "\033[35m#{e.inspect}\033[0m\n"
+      puts "\033[45m  >>> Several errors exist\033[0m\n"
+      begin # identiy kind of error: check if element has class '.is--required.has--error'
+        find(".is--required.has--error")
+      rescue Capybara::Ambiguous => e
+        puts "\033[45m  >>> several inputs are required\033[0m\n"
+        element = find(".is--required.has--error")
+        puts "\033[31m    >>>>>> #{element[:class]}\033[0m\n"
+      end
+    end
+  end
+
 
   # find: catch Errors
   def find_secure_with_two_args(path, string)
@@ -435,6 +442,27 @@ module MyFunctions
       raise "UNKNOWN ERROR in find_secure_with_two_args"
     end
     return found
+  end
+
+  # find: lazyloading
+  def find_element_with_scrolling(path)
+    # it is useful for our pdp because react uses lazyload
+    # it is a workaround to handle lazyload in Capybara/Selenium
+    # generate a loop without the template of a loop
+    # scoll down for 100px until Capybara the element which I am looking for
+    # counter to avoid an endless loop
+    find_scrolling_counter = 0
+    begin
+      # For faster 'loop' set lower Timeout
+      Capybara.default_max_wait_time = 1
+      find(path)
+    rescue Capybara::ElementNotFound
+      puts "> Maybe there is something like lazyloading active -> scroll down"
+      page.execute_script("window.scrollBy(0,100)")
+      # condition to exit the fuencton
+      find_scrolling_counter =+ 1
+      find_scrolling_counter < 20 ? retry : raise
+    end
   end
 
   # visit: catch Errors
@@ -538,6 +566,31 @@ module MyFunctions
     end
   end
 
+  def get_url_part_of_website(site_of)
+    #replace given string with url of this page
+    if(site_of)
+      case site_of
+      when "homepage"
+        puts "homepage"
+        url_part = ""
+      # it is not implemented. not yet!
+      # when "category"
+      #   puts "categorypage of #{websitebasics[:data].category}"
+      #   url_part = "#{websitebasics[:data].category}/".downcase
+      # when "subcategory"
+      #   puts "subcategorypage of #{websitebasics[:data].sub_category}"
+      #   url_part = "#{websitebasics[:data].category}/#{websitebasics[:data].sub_category}/".downcase
+      when "productdetailpage"
+        puts "subcategorypage of #{websitebasics[:data].productdetailpage}"
+        url_part = "test-addtocart-735707?number=7357-07-1"
+      else
+        puts "Options: \n -homepage\n- category \n- subcategory \ndefault: take the given string"
+        url_part = site_of
+      end
+      return url_part
+    end
+  end
+
   #check if url has changed
   def check_for_url_change(start_url)
   #check for success
@@ -592,7 +645,6 @@ module MyFunctions
       puts "expect_url:#{expect_url}"
       puts "url:#{url}"
       abort("WRONG URL:##{url}")
-      return "WRONG URL:##{url}"
     end
 
 
