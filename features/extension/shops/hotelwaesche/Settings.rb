19bf756ad127d083951e4e07dec62e523d9250f8
johannes112 <johannes.emmos@gmail.com>
2019-02-12 14:44:50 +0100

    config: settings -> http - https

--- features/extension/shops/hotelwaesche/Settings.rb
+++ features/extension/shops/hotelwaesche/Settings.rb
@@ -5,47 +5,47 @@ class Settings
   def initialize
     if ENV['SYSTEM'] == "int"
       @urlHttp = case ENV['COUNTRY']
-        when (/de/) then 'http://int.hotelwaesche.de/'
-        when (/at/) then 'http://int.hotelwaesche.at/'
-        when (/ch/) then 'http://int.erwinmueller.ch/ch-de/'
+        when (/de/) then    'http://int.hotelwaesche.de/'
+        when (/at/) then    'http://int.hotelwaesche.at/'
+        when (/ch/) then    'http://int.erwinmueller.ch/ch-de/'
         when (/ch-fr/) then 'http://int.erwinmueller.ch/ch-fr/'
       end
       @urlHttps = case ENV['COUNTRY']#Important:write 's
-        when (/de/) then 'http://int.hotelwaesche.de/'
-        when (/at/) then 'http://int.hotelwaesche.at/'
-        when (/ch/) then 'http://int.erwinmueller.ch/ch-de/'
-        when (/ch-fr/) then 'http://int.erwinmueller.ch/ch-fr/'
+        when (/de/) then    'https://int.hotelwaesche.de/'
+        when (/at/) then    'https://int.hotelwaesche.at/'
+        when (/ch/) then    'https://int.erwinmueller.ch/ch-de/'
+        when (/ch-fr/) then 'https://int.erwinmueller.ch/ch-fr/'
       end
       @urlBackend = 'http://int.hotelwaesche.de'
     end
 
     if ENV['SYSTEM'] == "stage"
       @urlHttp = case ENV['COUNTRY']
-      when (/de/) then    'http://stage.hotelwaesche.de/'
-      when (/at/) then    'http://stage.hotelwaesche.at/'
-      when (/ch/) then    'http://stage.erwinmueller.ch/ch-de/'
-      when (/ch-fr/) then 'http://stage.erwinmueller.ch/ch-fr/'
+        when (/de/) then    'http://stage.hotelwaesche.de/'
+        when (/at/) then    'http://stage.hotelwaesche.at/'
+        when (/ch/) then    'http://stage.erwinmueller.ch/ch-de/'
+        when (/ch-fr/) then 'http://stage.erwinmueller.ch/ch-fr/'
       end
       @urlHttps = case ENV['COUNTRY']#Important:write 's
-      when (/de/) then    'http://stage.hotelwaesche.de/'
-      when (/at/) then    'http://stage.hotelwaesche.at/'
-      when (/ch/) then    'http://stage.erwinmueller.ch/ch-de/'
-      when (/ch-fr/) then 'http://stage.erwinmueller.ch/ch-fr/'
+        when (/de/) then    'https://stage.hotelwaesche.de/'
+        when (/at/) then    'https://stage.hotelwaesche.at/'
+        when (/ch/) then    'https://stage.erwinmueller.ch/ch-de/'
+        when (/ch-fr/) then 'https://stage.erwinmueller.ch/ch-fr/'
       end
       @urlBackend = 'http://stage.hotelwaesche.de'
     end
 
     if ENV['SYSTEM'] == "live"
       @urlHttp = case ENV['COUNTRY']
-        when (/de/) then 'http://www.hotelwaesche.de/'
-        when (/at/) then 'http://www.hotelwaesche.at/'
-        when (/ch/) then 'http://www.erwinmueller.ch/ch-de/'
+        when (/de/) then    'http://www.hotelwaesche.de/'
+        when (/at/) then    'http://www.hotelwaesche.at/'
+        when (/ch/) then    'http://www.erwinmueller.ch/ch-de/'
         when (/ch-fr/) then 'http://www.erwinmueller.ch/ch-fr/'
       end
       @urlHttps = case ENV['COUNTRY']
-        when (/de/) then 'https://www.hotelwaesche.de/'
-        when (/at/) then 'https://www.hotelwaesche.at/'
-        when (/ch/) then 'https://www.erwinmueller.ch/ch-de/'
+        when (/de/) then    'https://www.hotelwaesche.de/'
+        when (/at/) then    'https://www.hotelwaesche.at/'
+        when (/ch/) then    'https://www.erwinmueller.ch/ch-de/'
         when (/ch-fr/) then 'https://www.erwinmueller.ch/ch-fr/'
       end
       @urlBackend = 'https://www.hotelwaesche.de'
--- features/extension/shops/hw_jobeline/Settings.rb
+++ features/extension/shops/hw_jobeline/Settings.rb
@@ -5,16 +5,16 @@ class Settings
   def initialize
     if ENV['SYSTEM'] == "int"
       @urlHttp = case ENV['COUNTRY']
-        when (/de/) then 'http://int.jobeline.de/'
-        when (/at/) then 'http://int.jobeline.at/'
-        when (/ch/) then 'http://int.jobeline.ch/ch-de/'
+        when (/de/) then    'http://int.jobeline.de/'
+        when (/at/) then    'http://int.jobeline.at/'
+        when (/ch/) then    'http://int.jobeline.ch/ch-de/'
         when (/ch-fr/) then 'http://int.jobeline.ch/ch-fr/'
       end
       @urlHttps = case ENV['COUNTRY']#Important:write 's
-        when (/de/) then 'http://int.jobeline.de/'
-        when (/at/) then 'http://int.jobeline.at/'
-        when (/ch/) then 'http://int.jobeline.ch/ch-de/'
-        when (/fr/) then 'http://int.jobeline.ch/ch-fr/'
+        when (/de/) then    'https://int.jobeline.de/'
+        when (/at/) then    'https://int.jobeline.at/'
+        when (/ch/) then    'https://int.jobeline.ch/ch-de/'
+        when (/fr/) then    'https://int.jobeline.ch/ch-fr/'
       end
       @urlBackend = 'http://int.jobeline.de/'
     end
@@ -27,25 +27,25 @@ class Settings
       when (/ch-fr/) then 'http://stage.jobeline.ch/ch-fr/'
       end
       @urlHttps = case ENV['COUNTRY']#Important:write 's
-      when (/de/) then    'http://stage.jobeline.de/'
-      when (/at/) then    'http://stage.jobeline.at/'
-      when (/ch/) then    'http://stage.jobeline.ch/ch-de/'
-      when (/ch-fr/) then 'http://stage.jobeline.ch/ch-fr/'
+      when (/de/) then    'https://stage.jobeline.de/'
+      when (/at/) then    'https://stage.jobeline.at/'
+      when (/ch/) then    'https://stage.jobeline.ch/ch-de/'
+      when (/ch-fr/) then 'https://stage.jobeline.ch/ch-fr/'
       end
       @urlBackend = 'http://stage.jobeline.de/'
     end
 
     if ENV['SYSTEM'] == "live"
       @urlHttp = case ENV['COUNTRY']
-        when (/de/) then 'http://www.jobeline.de/'
-        when (/at/) then 'http://www.jobeline.at/'
-        when (/ch/) then 'http://www.jobeline.ch/ch-de'
+        when (/de/) then    'http://www.jobeline.de/'
+        when (/at/) then    'http://www.jobeline.at/'
+        when (/ch/) then    'http://www.jobeline.ch/ch-de'
         when (/ch-fr/) then 'http://www.jobeline.ch/ch-fr'
       end
       @urlHttps = case ENV['COUNTRY']
-        when (/de/) then 'https://www.jobeline.de/'
-        when (/at/) then 'https://www.jobeline.at/'
-        when (/ch/) then 'https://www.jobeline.ch/ch-de/'
+        when (/de/) then    'https://www.jobeline.de/'
+        when (/at/) then    'https://www.jobeline.at/'
+        when (/ch/) then    'https://www.jobeline.ch/ch-de/'
         when (/ch-fr/) then 'https://www.jobeline.ch/ch-fr/'
       end
       @urlBackend = 'https://www.jobeline.de/'
