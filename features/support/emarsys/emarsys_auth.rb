cd3c9afc18c8715b253365d2eb5e76cdd93f0633
johannes112 <johannes.emmos@gmail.com>
2019-03-18 15:36:29 +0100

    use sfey_yaml and set_default_configuration to true (important)

--- features/support/emarsys/emarsys_auth.rb
+++ features/support/emarsys/emarsys_auth.rb
@@ -1,10 +1,15 @@
-require 'yaml'
+require 'safe_yaml'
+#require 'yaml'
 require 'emarsys'
 
+SafeYAML::OPTIONS[:default_mode] = :safe
+Emarsys.allow_default_configuration = true
 # Emarsys.allow_default_configuration = true
 e_accounts = YAML.load_file('emarsys_accounts.yml')
 
 Emarsys.configure do |c|
+  # c.api_username = e_accounts['hotelwaesche']['de']['user']
+  # c.api_password = e_accounts['hotelwaesche']['de']['pass']
   c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
   c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
 end
