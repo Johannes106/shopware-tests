0e388471c933cda8132238fa415a70a42e3eebf9
johannes112 <johannes.emmos@gmail.com>
2019-03-19 08:04:18 +0100

    put code into a class

--- features/support/emarsys/emarsys_auth.rb
+++ features/support/emarsys/emarsys_auth.rb
@@ -1,15 +1,21 @@
 require 'safe_yaml'
-#require 'yaml'
 require 'emarsys'
 
-SafeYAML::OPTIONS[:default_mode] = :safe
-Emarsys.allow_default_configuration = true
-# Emarsys.allow_default_configuration = true
-e_accounts = YAML.load_file('emarsys_accounts.yml')
+class Emarsys_auth
+  # configure SafeYAML
+  SafeYAML::OPTIONS[:default_mode] = :safe
 
-Emarsys.configure do |c|
-  # c.api_username = e_accounts['hotelwaesche']['de']['user']
-  # c.api_password = e_accounts['hotelwaesche']['de']['pass']
-  c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
-  c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
+  # use one password for each whole session/run
+  Emarsys.allow_default_configuration = true
+  # grep account data of yaml file
+  e_accounts = YAML.load_file('emarsys_accounts.yml')
+
+  # set user and password for session/run
+  Emarsys.configure do |c|
+    # use it in the emarsys dwith emarsys_api_main.rb (output/)
+    # c.api_username = e_accounts['hotelwaesche']['de']['user']
+    # c.api_password = e_accounts['hotelwaesche']['de']['pass']
+    c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
+    c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
+  end
 end
