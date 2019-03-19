690ded4c72ab8b976abed8c056b04d6c52964ba2
johannes112 <johannes.emmos@gmail.com>
2019-03-19 10:22:21 +0100

    change way to load the yaml-file -> now it is the right relative path

--- features/support/emarsys/emarsys_auth.rb
+++ features/support/emarsys/emarsys_auth.rb
@@ -8,13 +8,15 @@ class Emarsys_auth
   # use one password for each whole session/run
   Emarsys.allow_default_configuration = true
   # grep account data of yaml file
-  e_accounts = YAML.load_file('emarsys_accounts.yml')
+  # to get always the right path of the yaml
+  e_accounts = YAML::load_file(File.join(__dir__, 'emarsys_accounts.yml'))
 
   # set user and password for session/run
   Emarsys.configure do |c|
-    # use it in the emarsys dwith emarsys_api_main.rb (output/)
+    # use it in the emarsys dir with emarsys_api_main.rb (output/)
     # c.api_username = e_accounts['hotelwaesche']['de']['user']
     # c.api_password = e_accounts['hotelwaesche']['de']['pass']
+    # use it in the testhub
     c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
     c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
   end
