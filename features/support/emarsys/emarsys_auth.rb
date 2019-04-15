9ac64564333180315164e9c4088624ede5b9e020
johannes112 <johannes.emmos@gmail.com>
2019-04-15 13:46:40 +0200

    bugfix: instead of condtional loading of libaries use default dummy data

--- features/support/emarsys/emarsys_auth.rb
+++ features/support/emarsys/emarsys_auth.rb
@@ -1,18 +1,21 @@
 require 'safe_yaml'
 require 'emarsys'
 
-#puts "Emarsys_auth:#{yaml_exists}"
-if ($yaml_exists)
-  class Emarsys_auth
-    # configure SafeYAML
-    SafeYAML::OPTIONS[:default_mode] = :safe
 
-    # rel path
-    backward_path = '../.' #go back -> emarsys/support/features/workspace
-    rel_path = File.join(File.expand_path(backward_path), 'emarsys_accounts.yml')
-    yaml_exists = File.file?(rel_path)
-    # rel_path = File.join(__dir__, 'emarsys_accounts.yml')
 
+class Emarsys_auth
+  # configure SafeYAML
+  SafeYAML::OPTIONS[:default_mode] = :safe
+
+  # rel path
+  backward_path = '../.' #go back -> emarsys/support/features/workspace
+  rel_path = File.join(File.expand_path(backward_path), 'emarsys_accounts.yml')
+  yaml_exists = File.file?(rel_path)
+  if (yaml_exists)
+    $yaml_exists = true
+  end
+  # rel_path = File.join(__dir__, 'emarsys_accounts.yml')
+  if ($yaml_exists)
     # use one password for each whole session/run
     Emarsys.allow_default_configuration = true
     # grep account data of yaml file
@@ -26,6 +29,16 @@ if ($yaml_exists)
       # use it in the testhub
       c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
       c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
+    end
+  else
+    puts "Load Dummy data"
+    Emarsys.configure do |c|
+      # use it in the emarsys dir with emarsys_api_main.rb (output/)
+      # c.api_username = e_accounts['hotelwaesche']['de']['user']
+      # c.api_password = e_accounts['hotelwaesche']['de']['pass']
+      # use it in the testhub
+      c.api_username = 'username'
+      c.api_password = 'password'
     end
   end
 end
