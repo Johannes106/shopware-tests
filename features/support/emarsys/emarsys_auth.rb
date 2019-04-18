bb7ea15c369a610f04c55518fe2732a7c269c5f2
johannes112 <johannes.emmos@gmail.com>
2019-04-18 09:33:22 +0200

    prevent error thrown of empty array -> set default values if array is empty

--- features/support/emarsys/emarsys_auth.rb
+++ features/support/emarsys/emarsys_auth.rb
@@ -5,31 +5,37 @@ class Emarsys_auth
   yaml_exists = File.file?(rel_path)
   if (yaml_exists)
     $yaml_exists = true
+  else
+    puts "!!! There should be a yaml on #{rel_path}"
   end
   # if there are account data
   if ($yaml_exists)
     require 'safe_yaml'
-
+    shop = ENV['SHOP']
+    country = ENV['COUNTRY']
     # configure SafeYAML
     SafeYAML::OPTIONS[:default_mode] = :safe
     # use one password for each whole session/run
     Emarsys.allow_default_configuration = true
     # grep account data of yaml file
     # to get always the right path of the yaml
+    # be careful yaml::load_file interprets strings without ''
     e_accounts = YAML::load_file(rel_path)
     # set user and password for session/run
     Emarsys.configure do |c|
       # use it in the emarsys dir with emarsys_api_main.rb (output/)
       # c.api_username = e_accounts['hotelwaesche']['de']['user']
       # c.api_password = e_accounts['hotelwaesche']['de']['pass']
-      # if the environment-vars are existing
-      if (ENV['SHOP']) && (ENV['COUNTRY'])
-        c.api_username = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['user']
-        c.api_password = e_accounts[(ENV['SHOP'])][(ENV['COUNTRY'])]['pass']
+      # if the environment-vars are existing and the yaml contain the values
+      #if ((shop)) && ((country))
+      if (shop) && (country)
+        c.api_username = e_accounts[shop][country]['user']
+        c.api_password = e_accounts[shop][country]['pass']
       else
-      # use default data and get an error (Unauthorized)
-        c.api_username = 'username'
-        c.api_password = 'password'
+        puts "> YAML exists but the values '#{shop}' and '#{country}' aren't included"
+        puts "> use default data and get an error (Unauthorized)"
+        c.api_username = 'username_not_defined'
+        c.api_password = 'password_not_defined'
       end
     end
   end
