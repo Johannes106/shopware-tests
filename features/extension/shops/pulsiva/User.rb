7d249beb75144e953ba6a57d16478545a477734e
johannes112 <johannes.emmos@gmail.com>
2019-05-10 12:07:36 +0200

    fix: pfr -> add payment_option 'Scheck'

--- features/extension/shops/pulsiva/User.rb
+++ features/extension/shops/pulsiva/User.rb
@@ -298,7 +298,7 @@ class User
       when 'at' then ['Rechnung', 'Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal']
       when 'ch' then ['Rechnung', 'Nachnahme', 'Kreditkarte', 'Vorauskasse', 'Paypal']
       when 'it' then ['Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal']
-      when 'fr' then ['Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal']
+      when 'fr' then ['Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal', 'Scheck']
       when 'no' then ['Nachnahme', 'Kreditkarte', 'Vorkasse']
       when 'se' then ['Nachnahme', 'Kreditkarte', 'Vorkasse']
       when 'nl' then ['Rechnung', 'Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal', 'iDeal']
