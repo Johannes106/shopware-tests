02bad47129d5c231932e0e1ab0770940f6db8906
johannes112 <johannes.launer@emmos.de>
2019-06-17 14:55:59 +0200

    nothing to say

--- features/support/helpers/javascript_functions.rb
+++ features/support/helpers/javascript_functions.rb
@@ -1,11 +1,11 @@
 #Here are all javascript-functions that are saved in ruby variables
 class Jsfunctions
   attr_accessor :disableStylesheet, :refreshPage
-  
+
   def initialize
-    
+
     @disableStylesheet="document.styleSheets[0].disabled = !document.styleSheets[0].disabled;"
-  
-    @refreshPage="driver.navigate.refresh"    
+
+    @refreshPage="location.reload();"    
   end
 end
