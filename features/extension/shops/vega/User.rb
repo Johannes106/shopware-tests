a80d7f3983ae8912a7bd76588d4ae3dd84e7fbea
johannes112 <johannes.launer@emmos.de>
2019-07-03 14:42:26 +0200

    user.rb: payments
    VEN & VJEN -> remove paypal as a paymentoption

--- features/extension/shops/vega/User.rb
+++ features/extension/shops/vega/User.rb
@@ -375,7 +375,7 @@ class User
       when 'nl' then ['Rechnung', 'Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal', 'iDeal']
       when 'es' then ['Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal']
       when 'be' then ['Nachnahme', 'Kreditkarte', 'Vorkasse', 'Paypal']
-      when 'en' then ['Nachnahme', 'Paypal', 'Vorkasse']
+      when 'en' then ['Kreditkarte', 'Vorkasse']
     end
   end
 
