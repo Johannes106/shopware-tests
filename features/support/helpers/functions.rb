35aaa2ef9ba7ef64c0a8f02265e1d66e9323a918
johannes112 <johannes.launer@emmos.de>
2020-04-22 09:55:55 +0200

    close popup: integrate close_popup(path, time) within visit_secure to close the modal of b2c

--- features/support/helpers/functions.rb
+++ features/support/helpers/functions.rb
@@ -503,6 +503,9 @@ module MyFunctions
       visit_secure_counter += 1
       visit(url)
       puts "go to #{url}"
+      if ( (ENV['SHOP']=='pulsiva') || (ENV['SHOP']=='v_jobeline') || (ENV['SHOP']=='hotelwaesche') || (ENV['SHOP']=='vega') && (ENV['COUNTRY']=='de') || (ENV['COUNTRY']=='at') || (ENV['COUNTRY']=='ch') )
+        close_popup(".modal--close", 5)
+      end
       if ( ENV['SHOP']=='vega' && (ENV['COUNTRY']=='de') || (ENV['COUNTRY']=='it') )
         block_css('.dpe-shopwide')
       end
